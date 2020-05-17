28684b4307cc8a20c230094508d1b4364db680f3
Abbas Egbeyemi <abbasegbeyemi@gmail.com>
2020-05-17 12:46:10 +0100

    Placeholder page sorted.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faExternalLinkAlt, faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import {faLinkedinIn, faGithub, faTwitter} from '@fortawesome/free-brands-svg-icons'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            At some point we'll graduate from a react app.
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Personal Website for Dr Abbas Egbeyemi.
+                </p>
+                <div className="contact-button">
+                    <span><FontAwesomeIcon icon={faExternalLinkAlt}/> Contact me </span>
+                    <a href="#"><FontAwesomeIcon icon={faLinkedinIn}/></a>
+                    <a href="#"><FontAwesomeIcon icon={faGithub}/></a>
+                    <a href="#"><FontAwesomeIcon icon={faTwitter}/></a>
+                    <a href="#"><FontAwesomeIcon icon={faEnvelope}/></a>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
